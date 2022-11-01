4d8c39f373b1efe9c98a056f3c489852d5fff677
David Urbano <david.urbano_code@outlook.com>
2022-11-01 18:40:36 -0500

    Still working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import './../styles/Main.css';
+
 const Main = () => {
 	return (
 		<>
@@ -10,34 +12,57 @@ const Main = () => {
 						<h2 id='student-info'>Student Info</h2>
 						<div className='info'>
 							<label for='name'>Name:</label>
-							<input
-								name='student-info'
-								placeholder='text'
-								id='name'
-								type='text'
-							></input>
+							<input name='student-info' id='name' type='text'></input>
 						</div>
 						<div className='info'>
 							<label for='email'>Email:</label>
-							<input
-								name='student-email'
-								placeholder='text'
-								id='email'
-								type='email'
-							></input>
+							<input name='student-email' id='email' type='email'></input>
 						</div>
 						<div className='info'>
-							<label for='date'>D.O.B.:</label>
-							<input
-								name='birthday'
-								placeholder='text'
-								id='date'
-								type='date'
-							></input>
+							<label for='date'>
+								D.O.B.: <span className='sr-only'>(Date of Birth)</span>
+							</label>
+							<input name='birthday' id='date' type='date'></input>
 						</div>
 					</section>
 					<section role='region' aria-labelledby='html-questions'>
 						<h2 id='html-questions'>HTML</h2>
+						<div className='question-block'>
+							<p>1</p>
+							<fieldset name='1' className='question'>
+								<legend>yes/no</legend>
+								<ul className='question'>
+									<li>
+										<label>
+											<input id='q1-a1' type='radio'></input>
+										</label>
+									</li>
+									<li>
+										<label>
+											<input id='q1-a2' type='radio'></input>
+										</label>
+									</li>
+								</ul>
+							</fieldset>
+						</div>
+						<div className='question-block'>
+							<p>1</p>
+							<fieldset name='2' className='question'>
+								<legend>true/false</legend>
+								<ul className='question'>
+									<li>
+										<label>
+											<input id='q2-a1' type='radio'></input>
+										</label>
+									</li>
+									<li>
+										<label>
+											<input id='q2-a2' type='radio'></input>
+										</label>
+									</li>
+								</ul>
+							</fieldset>
+						</div>
 					</section>
 					<section role='region' aria-labelledby='css-questions'>
 						<h2 id='css-questions'>CSS</h2>
