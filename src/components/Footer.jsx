6dab5d4f48769ad1441d66003a1f63d7e640a019
David Urbano <david.urbano_code@outlook.com>
2022-11-02 16:30:44 -0500

    Is taking long

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import './../styles/Footer.css';
+
+const Footer = () => {
+	return (
+		<>
+			<footer>
+				<address>
+					<a href='https://freecodecamp.org'>freeCodeCamp</a>
+					<br />
+					San Francisco
+					<br />
+					California
+					<br />
+					USA
+				</address>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
