f45f517b2eb26f8f5a6011ce36f0db72d52dbce5
David Urbano <david.urbano_code@outlook.com>
2022-11-02 00:21:54 -0500

    almost

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,13 +33,25 @@ const Main = () => {
 								<legend>yes/no</legend>
 								<ul className='question'>
 									<li>
-										<label>
-											<input id='q1-a1' type='radio'></input>
+										<label for='q1-a1'>
+											<input
+												name='q2'
+												value='True'
+												id='q1-a1'
+												type='radio'
+											></input>
+											True
 										</label>
 									</li>
 									<li>
-										<label>
-											<input id='q1-a2' type='radio'></input>
+										<label for='q1-a2'>
+											<input
+												name='q2'
+												value='False'
+												id='q1-a2'
+												type='radio'
+											></input>
+											False
 										</label>
 									</li>
 								</ul>
@@ -51,13 +63,25 @@ const Main = () => {
 								<legend>true/false</legend>
 								<ul className='question'>
 									<li>
-										<label>
-											<input id='q2-a1' type='radio'></input>
+										<label for='q2-a1'>
+											<input
+												name='q1'
+												value='True'
+												id='q2-a1'
+												type='radio'
+											></input>
+											True
 										</label>
 									</li>
 									<li>
-										<label>
-											<input id='q2-a2' type='radio'></input>
+										<label name='q2' for='q2-a2'>
+											<input
+												name='q1'
+												value='False'
+												id='q2-a2'
+												type='radio'
+											></input>
+											False
 										</label>
 									</li>
 								</ul>
@@ -66,7 +90,32 @@ const Main = () => {
 					</section>
 					<section role='region' aria-labelledby='css-questions'>
 						<h2 id='css-questions'>CSS</h2>
+						<div className='formrow'>
+							<div className='question-block'>
+								<label for='customer'>Are you a frontend developer?</label>
+							</div>
+							<div className='answer'>
+								<select name='customer' id='customer' required>
+									<option value=''>Select an option</option>
+									<option value='yes'>Yes</option>
+									<option value='no'>No</option>
+								</select>
+							</div>
+							<div className='question-block'>
+								<label for='css-questions'>Do you have any questions:</label>
+							</div>
+							<div className='answer'>
+								<textarea
+									id='css-questions'
+									name='css-questions'
+									rows='5'
+									cols='24'
+									placeholder='Who is flexbox...'
+								></textarea>
+							</div>
+						</div>
 					</section>
+					<button type='submit'>Submit</button>
 				</form>
 			</main>
 		</>
